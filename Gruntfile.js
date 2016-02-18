b28eb2886fbcd0c53bf0d015502cd776c01847ad
savchenkoa <anton.savchenko@studentuniverse.com>
2016-02-18 17:22:46 -0500

    add html2js support

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,11 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     concat: {
       options: {
-        banner: '(function(){\n\'use strict;\'\n',
+        banner: '(function(){\n\'use strict\';\n',
         footer: '})();'
       },
       build: {
-        src: ['src/su/datepicker/**/*.js'],
+        src: ['src/su/datepicker/**/*.js', 'tmp/templates.js'],
         dest: 'build/su-datepicker.js',
       },
     },
@@ -25,15 +25,39 @@ module.exports = function(grunt) {
         files: ['src/**/*.js'],
         tasks: ['build']
       }
+    },
+    html2js: {
+      options: {
+        module: 'su.datepicker.templates.suDatepickerTemplatesModule',
+        rename: function (moduleName) {
+          return moduleName.replace('.html', '').replace(/\//g, '.');
+        },
+        quoteChar: '\'',
+        htmlmin: {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          removeAttributeQuotes: true,
+          removeComments: true,
+          removeEmptyAttributes: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true
+        }
+      },
+      main: {
+        src: ['src/**/*.html'],
+        dest: 'tmp/templates.js'
+      },
     }
   });
 
   //grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-html2js');
   grunt.loadNpmTasks('grunt-karma');
 
-  grunt.registerTask('build', ['concat']);
+  grunt.registerTask('build', ['html2js', 'concat']);
   grunt.registerTask('default', ['build', 'watch']);
 
   grunt.registerTask('test', ['build', 'karma:unit']);
