f0f5148f990d42abaeb64c7c860cf777d2b42456
savchenkoa <anton.savchenko@studentuniverse.com>
2016-02-18 17:37:39 -0500

    add suDatepickerDirectivesModule

---
+++ src/su/datepicker/directives/suDatepickerDirectivesModule.js
@@ -0,0 +1,5 @@
+angular.module('su.datepicker.directives.suDatepickerDirectivesModule', [
+  'su.datepicker.directives.suBasicDatepicker',
+  'su.datepicker.directives.suDatepickerDayDirective',
+  'su.datepicker.directives.suDatepickerMonthDirective'
+]);
