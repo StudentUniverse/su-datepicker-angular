8dc02dfb9007a0059c9225eb2a455a7e5f88fa6b
savchenkoa <anton.savchenko@studentuniverse.com>
2016-02-25 14:09:43 -0500

    update directives module

--- src/su/datepicker/directives/suDatepickerDirectivesModule.js
+++ src/su/datepicker/directives/suDatepickerDirectivesModule.js
@@ -1,7 +1,9 @@
 angular.module('su.datepicker.directives.suDatepickerDirectivesModule', [
   'su.datepicker.directives.suDatepickerCheapEventDirective',
+  'su.datepicker.directives.suDatepickerDateParserDirective',
   'su.datepicker.directives.suDatepickerDayDirective',
   'su.datepicker.directives.suDatepickerDefaultDirective',
   'su.datepicker.directives.suDatepickerMonthDefaultDirective',
-  'su.datepicker.directives.suDatepickerMonthDirective'
+  'su.datepicker.directives.suDatepickerMonthDirective',
+  'su.datepicker.directives.suDatepickerRangeDefaultDirective',
 ]);
