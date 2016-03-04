a9b2e8ad09eba8816f451b551f41d50ce06d37e6
savchenkoa <anton.savchenko@studentuniverse.com>
2016-03-04 16:42:21 -0500

    allow multiple cheap events

--- src/su/datepicker/directives/suDatepickerCheapEventDirective.js
+++ src/su/datepicker/directives/suDatepickerCheapEventDirective.js
@@ -3,19 +3,32 @@ angular.module('su.datepicker.directives.suDatepickerCheapEventDirective', [])
 
 // do not depend on this directive, it may be removed in the future
 suDatepickerCheapEventDirective.$inject = ['$parse'];
+
 function suDatepickerCheapEventDirective($parse) {
   return {
     compile: function(element, attrs) {
       var dirAttr = 'su-datepicker-cheap-event';
-      var event = attrs[attrs.$normalize(dirAttr)];
-      var eventExpressionAttr = dirAttr + '-' + event;
-      var eventFunction = $parse(attrs[attrs.$normalize(eventExpressionAttr)]);
+      var events = attrs[attrs.$normalize(dirAttr)].split(',');
+      var callbacks = {};
+      events.forEach(function(event) {
+        var eventExpressionAttr = dirAttr + '-' + event;
+        callbacks[event] = $parse(attrs[attrs.$normalize(eventExpressionAttr)]);
+      });
+
       return function postLink(scope, element) {
-        element.on(event, function(event) {
-          eventFunction(scope, {
-            $event: event
-          });
-        });
+        for (var eventName in callbacks) {
+          element.on(eventName, getCallback(eventName));
+        }
+
+        function getCallback(eventName) {
+          return function(event) {
+            var eventCallback = callbacks[eventName];
+            eventCallback(scope, {
+              $event: event
+            });
+          };
+        }
+
       };
     }
   };
