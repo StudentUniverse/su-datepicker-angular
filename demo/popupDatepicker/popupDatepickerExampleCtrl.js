0e691d40a00290f36d87907bc58d2cd1676c8437
savchenkoa <anton.savchenko@studentuniverse.com>
2016-03-14 15:00:42 -0400

    popup demo styles

--- demo/popupDatepicker/popupDatepickerExampleCtrl.js
+++ demo/popupDatepicker/popupDatepickerExampleCtrl.js
@@ -3,6 +3,16 @@ function popupDatepickerExampleCtrl($scope, $document){
   $scope.date = undefined;
   $scope.shown = false;
 
+  $scope.customClass = function(date){
+    if (angular.isDate(date) && angular.isDate($scope.date)) {
+      if ($scope.date.getFullYear() === date.getFullYear() &&
+      $scope.date.getMonth() === date.getMonth() &&
+      $scope.date.getDate() === date.getDate()) {
+        return 'active-date';
+      }
+    }
+  };
+
   $scope.showDatepicker = function(needsDigest){
     if(!$scope.shown){
       $scope.shown = true;
