00bc41315923708097581d909d3cb8af493b699f
moringm <matthew.moring@studentuniverse.com>
2016-03-24 16:58:47 -0400

    removed table and button paddings, swapped in SU prev and next arrows as inline svgs

--- src/su/datepicker/directives/suDatepickerChangeButtonDirective.js
+++ src/su/datepicker/directives/suDatepickerChangeButtonDirective.js
@@ -4,8 +4,8 @@ angular.module('su.datepicker.directives.suDatepickerChangeButtonDirective', [])
 suDatepickerChangeButtonDirective.$inject = [];
 function suDatepickerChangeButtonDirective(){
   var BUTTON_TEXT = {
-    previous: '&laquo',
-    next: '&raquo',
+    previous: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 190.625 190.625" class="su-prev-arrow"><path d="M91.85 8.398l8.615 8.616L28.38 89.1h156.874v12.184H28.38l72.085 72.086-8.616 8.614L5.055 95.19"></svg>',
+    next: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 190.625 190.625" class="su-next-arrow"><path d="M185.255 95.19l-86.793 86.795-8.617-8.615 72.086-72.086H5.057V89.1H161.93L89.846 17.014l8.617-8.616"/></svg>',
   };
 
   var CLASS_MAP = {
